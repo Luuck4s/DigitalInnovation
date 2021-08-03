5150f4602421b8bb8dea86e6eb971e23394c2fc0
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2021-08-02 21:54:31 -0300

    [update] Adicionar projeto Sistema baseado em microsserviços

---
+++ SantanderFullStack/MicrosservicosSpringCloud/products-catalog/src/main/java/one/digitalinnovation/experts/productscatalog/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.experts.productscatalog.repository;
+
+import one.digitalinnovation.experts.productscatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByName(String name);
+}
