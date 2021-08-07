ded6ba509cd6bbd7ab5ad864a8badc0a11ba9a90
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2021-08-07 18:42:20 -0300

    [update] Adicionar curso de introdução angular

---
+++ SantanderFullStack/IntroducaoAngular/course-manager/src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course {
+  id!: number;
+  name!: String;
+  description!: String;
+  imageUrl!: string;
+  code!: string;
+  price!: number;
+  duration!: number;
+  rating!: number;
+  releaseDate!: string;
+}
